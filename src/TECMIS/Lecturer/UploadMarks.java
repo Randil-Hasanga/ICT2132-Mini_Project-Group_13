74038184c40c6a19c02574b46e313207eae8ecfc
Randil-Hasanga <randil_2020690@fot.ruh.ac.lk>
2023-03-15 22:00:53 +0530

    Done some improvements to viewGrade and GPA
    Database updated
    Now lecturer can view student grades in different views

--- src/TECMIS/Lecturer/UploadMarks.java
+++ src/TECMIS/Lecturer/UploadMarks.java
@@ -38,6 +38,7 @@ public class UploadMarks extends Lecturer{
     private JLabel lblFinalPractical;
     private JLabel lblFinalTheory;
 
+
     private String userId;
     private String acc;
 
@@ -50,15 +51,14 @@ public class UploadMarks extends Lecturer{
     double F_Theory;
     double F_Practical;
 
+
     double A1;
     double A2;
     private String selected;
 
 
 
-    public UploadMarks() {
 
-    }
 
     public void upMarks(){
 
@@ -253,34 +253,33 @@ public class UploadMarks extends Lecturer{
                 F_Practical = (txtFinalPractical.getText().isEmpty() || txtFinalPractical.getText() == null) ? Double.NaN : Double.parseDouble(txtFinalPractical.getText());
 
 
-                String upMarks = "INSERT INTO Exam_mark (Mark_id,Student_id,Course_id,Lecturer_id,Assignment001,Assignment002,QUIZ01,QUIZ02,QUIZ03,MID,FINAL_Practical,FINAL_Theory) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+                String upMarks = "INSERT INTO Exam_mark (Student_id,Course_id,Lecturer_id,Assignment001,Assignment002,QUIZ01,QUIZ02,QUIZ03,MID,FINAL_Practical,FINAL_Theory) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
                 try(PreparedStatement stmt = conn.prepareStatement(upMarks)){
-                    stmt.setNull(1, Types.INTEGER);
-                    stmt.setString(2,SID);
-                    stmt.setString(3,CID);
-                    stmt.setString(4,userId);
+                    stmt.setString(1,SID);
+                    stmt.setString(2,CID);
+                    stmt.setString(3,userId);
                     if((Double.isNaN(A1))||(Double.isNaN(A2))){
+                        stmt.setNull(4, Types.DECIMAL);
                         stmt.setNull(5, Types.DECIMAL);
-                        stmt.setNull(6, Types.DECIMAL);
                     }else{
-                        stmt.setDouble(5,A1);
-                        stmt.setDouble(6,A2);
+                        stmt.setDouble(4,A1);
+                        stmt.setDouble(5,A2);
                     }
-                    stmt.setDouble(7,Q1);
-                    stmt.setDouble(8,Q2);
-                    stmt.setDouble(9,Q3);
+                    stmt.setDouble(6,Q1);
+                    stmt.setDouble(7,Q2);
+                    stmt.setDouble(8,Q3);
                     if (Double.isNaN(MID)) {
-                        stmt.setNull(10, Types.DECIMAL);
+                        stmt.setNull(9, Types.DECIMAL);
                     }else{
-                        stmt.setDouble(10,MID);
+                        stmt.setDouble(9,MID);
                     }
 
                     if(Double.isNaN(F_Practical)){
-                        stmt.setNull(11, Types.DECIMAL);
+                        stmt.setNull(10, Types.DECIMAL);
                     }else{
-                        stmt.setDouble(11,F_Practical);
+                        stmt.setDouble(10,F_Practical);
                     }
-                    stmt.setDouble(12,F_Theory);
+                    stmt.setDouble(11,F_Theory);
 
 
                     int rowsInserted = stmt.executeUpdate();
@@ -292,138 +291,193 @@ public class UploadMarks extends Lecturer{
                 } catch (SQLException ex) {
                     throw new RuntimeException(ex);
                 }
+                updateExamMarks();
 
-                // getting 2 highest values from 3 quiz mark columns and insert it in new column
-
-                String upDateQuiz = "Update Exam_Mark SET Quiz_final = (GREATEST(QUIZ01,QUIZ02,QUIZ03) + LEAST(GREATEST(QUIZ01,QUIZ02),GREATEST(QUIZ01,QUIZ03),GREATEST(QUIZ02,QUIZ03)))/2";
-
-
-                try(Statement stmt = conn.createStatement()){
-
-                    stmt.executeUpdate(upDateQuiz);
-
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                String DSA = "UPDATE Exam_Mark SET final_mark = Quiz_final + MID + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT01'";
-
-                try(Statement stmt2 = conn.createStatement()){
-                    stmt2.executeUpdate(DSA);
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                String EC = "UPDATE Exam_Mark SET final_mark = Quiz_final + ((Assignment001 + Assignment001)/2) + MID + FINAL_Theory WHERE Course_id = 'ICT02'";
-
-                try(Statement stmt3 = conn.createStatement()){
-                    stmt3.executeUpdate(EC);
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                String SE = "UPDATE Exam_Mark SET final_mark = Quiz_final + Assignment001 + Assignment001 + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT03'";
-
-                try(Statement stmt4 = conn.createStatement()){
-                    stmt4.executeUpdate(SE);
-
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                String OOP = "UPDATE Exam_Mark SET final_mark = Quiz_final + Assignment001 + Assignment001 + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT04'";
-
-                try(Statement stmt5 = conn.createStatement()){
-                    stmt5.executeUpdate(OOP);
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                String WEB = "UPDATE Exam_Mark SET final_mark = Quiz_final + MID + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT05'";
-
-                try(Statement stmt6 = conn.createStatement()){
-                    stmt6.executeUpdate(WEB);
-
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                String BE = "UPDATE Exam_Mark SET final_mark = Quiz_final + ((Assignment001 + Assignment001)/2) + MID + FINAL_Theory WHERE Course_id = 'ICT06'";
-
-                try(Statement stmt7 = conn.createStatement()){
-
-                    stmt7.executeUpdate(BE);
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                // Updating final marks of each subject
-        /*
-                if(selected.equals("Data Structures and Algorithms")){
-                    String DSA = "UPDATE Exam_Mark SET final_mark = Quiz_final + MID + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT02'";
-
-                    try(Statement stmt2 = conn.createStatement()){
-                        stmt2.executeUpdate(DSA);
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
-                    }
-
-                }else if(selected.equals("E-Commerce")){
-                    String EC = "UPDATE Exam_Mark SET final_mark = Quiz_final + ((Assignment001 + Assignment001)/2) + MID + FINAL_Theory WHERE Course_id = 'ICT02'";
-
-                    try(Statement stmt3 = conn.createStatement()){
-                        stmt3.executeUpdate(EC);
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
-                    }
-
-                }else if(selected.equals("Software Engineering")) {
-
-                    String SE = "UPDATE Exam_Mark SET final_mark = Quiz_final + Assignment001 + Assignment001 + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT03'";
-
-                    try(Statement stmt4 = conn.createStatement()){
-                        stmt4.executeUpdate(SE);
-
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
-                    }
-
-                }else if(selected.equals("Object Oriented Programming")){
-
-                    String OOP = "UPDATE Exam_Mark SET final_mark = Quiz_final + Assignment001 + Assignment001 + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT04'";
-
-                    try(Statement stmt5 = conn.createStatement()){
-                        stmt5.executeUpdate(OOP);
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }
-                else if(selected.equals("Web technologies")){
-                    String WEB = "UPDATE Exam_Mark SET final_mark = Quiz_final + MID + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT05'";
-
-                    try(Statement stmt6 = conn.createStatement()){
-                        stmt6.executeUpdate(WEB);
-
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }else if(selected.equals("Business economics")) {
-
-                    String BE = "UPDATE Exam_Mark SET final_mark = Quiz_final + ((Assignment001 + Assignment001)/2) + MID + FINAL_Theory WHERE Course_id = 'ICT06'";
-
-                    try(Statement stmt7 = conn.createStatement()){
-
-                        stmt7.executeUpdate(BE);
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
-                    }
-
-                }*/
             }
-
-
-
         });
+    }
+    public void updateExamMarks(){
+        // getting 2 highest values from 3 quiz mark columns and insert it in new column
+        String upDateQuiz = "Update Exam_Mark SET Quiz_final = (GREATEST(QUIZ01,QUIZ02,QUIZ03) + LEAST(GREATEST(QUIZ01,QUIZ02),GREATEST(QUIZ01,QUIZ03),GREATEST(QUIZ02,QUIZ03)))/2";
+
+
+        try(Statement stmt = conn.createStatement()){
+
+            stmt.executeUpdate(upDateQuiz);
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        // Updating final marks,final ca, Grade letter and CA eligibility of each subject
+
+        String DSA = "UPDATE Exam_Mark SET final_mark = Quiz_final + MID + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT01'";
+        String fCA = "UPDATE Exam_mark SET final_ca = Quiz_final + MID WHERE Course_id = 'ICT01'";
+        String eg6 = "UPDATE Exam_mark SET eg = " +
+                "CASE " +
+                "WHEN final_ca >= 15 THEN 'CA Eligible' " +
+                "ELSE 'CA Not Eligible' " +
+                "END " +
+                "WHERE Course_id = 'ICT01'";
+
+        try(Statement stmt2 = conn.createStatement()){
+            Statement e3 = conn.createStatement();
+            Statement ca1 = conn.createStatement();
+            stmt2.executeUpdate(DSA);
+            ca1.executeUpdate(fCA);
+            e3.executeUpdate(eg6);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        String EC = "UPDATE Exam_Mark SET final_mark = Quiz_final + ((Assignment001 + Assignment002)/2) + MID + FINAL_Theory WHERE Course_id = 'ICT02'";
+        String fCA2 = "UPDATE Exam_mark SET final_ca = Quiz_final + ((Assignment001 + Assignment002)/2) + MID WHERE Course_id = 'ICT02'";
+        String eg1 = "UPDATE Exam_mark SET eg = " +
+                "CASE " +
+                "WHEN final_ca >= 10 THEN 'CA Eligible' " +
+                "ELSE 'CA Not Eligible' " +
+                "END " +
+                "WHERE Course_id = 'ICT02'";
+        try(Statement stmt3 = conn.createStatement()){
+            Statement e2 = conn.createStatement();
+            Statement ca2 = conn.createStatement();
+            stmt3.executeUpdate(EC);
+            ca2.executeUpdate(fCA2);
+            e2.executeUpdate(eg1);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        String SE = "UPDATE Exam_Mark SET final_mark = Quiz_final + Assignment001 + Assignment002 + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT03'";
+        String fCA3 = "UPDATE Exam_Mark SET final_ca = Quiz_final + Assignment001 + Assignment002 WHERE Course_id = 'ICT03'";
+        String eg = "UPDATE Exam_mark SET eg = " +
+                "CASE " +
+                "WHEN final_ca >= 15 THEN 'CA Eligible' " +
+                "ELSE 'CA Not Eligible' " +
+                "END " +
+                "WHERE Course_id = 'ICT03'";
+
+        try(Statement stmt4 = conn.createStatement()){
+            Statement ca3 = conn.createStatement();
+            Statement e1 = conn.createStatement();
+            stmt4.executeUpdate(SE);
+            ca3.executeUpdate(fCA3);
+            e1.executeUpdate(eg);
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        String OOP = "UPDATE Exam_Mark SET final_mark = Quiz_final + Assignment001 + Assignment001 + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT04'";
+        String fCA4 = "UPDATE Exam_Mark SET final_ca = Quiz_final + Assignment001 + Assignment002 WHERE Course_id = 'ICT04'";
+        String eg2 = "UPDATE Exam_mark SET eg = " +
+                "CASE " +
+                "WHEN final_ca >= 15 THEN 'CA Eligible' " +
+                "ELSE 'CA Not Eligible' " +
+                "END " +
+                "WHERE Course_id = 'ICT04'";
+        try(Statement stmt5 = conn.createStatement()){
+            Statement e4 = conn.createStatement();
+            Statement ca4 = conn.createStatement();
+            stmt5.executeUpdate(OOP);
+            ca4.executeUpdate(fCA4);
+            e4.executeUpdate(eg2);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        String WEB = "UPDATE Exam_Mark SET final_mark = Quiz_final + MID + FINAL_Theory + FINAL_Practical WHERE Course_id = 'ICT05'";
+        String fCA5 = "UPDATE Exam_mark SET final_ca = Quiz_final + MID WHERE Course_id = 'ICT05'";
+        String eg3 = "UPDATE Exam_mark SET eg = " +
+                "CASE " +
+                "WHEN final_ca >= 15 THEN 'CA Eligible' " +
+                "ELSE 'CA Not Eligible' " +
+                "END " +
+                "WHERE Course_id = 'ICT05'";
+        try(Statement stmt6 = conn.createStatement()){
+            Statement e5 = conn.createStatement();
+            Statement ca5 = conn.createStatement();
+            stmt6.executeUpdate(WEB);
+            ca5.executeUpdate(fCA5);
+            e5.executeUpdate(eg3);
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        String BE = "UPDATE Exam_Mark SET final_mark = Quiz_final + ((Assignment001 + Assignment002)/2) + MID + FINAL_Theory WHERE Course_id = 'ICT06'";
+        String fCA6 = "UPDATE Exam_mark SET final_ca = Quiz_final + ((Assignment001 + Assignment002)/2) + MID WHERE Course_id = 'ICT06'";
+        String eg4 = "UPDATE Exam_mark SET eg = " +
+                "CASE " +
+                "WHEN final_ca >= 10 THEN 'CA Eligible' " +
+                "ELSE 'CA Not Eligible' " +
+                "END " +
+                "WHERE Course_id = 'ICT06'";
+        try(Statement stmt7 = conn.createStatement()){
+            Statement e6 = conn.createStatement();
+            Statement ca6 = conn.createStatement();
+            stmt7.executeUpdate(BE);
+            ca6.executeUpdate(fCA6);
+            e6.executeUpdate(eg4);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+
+        String grade = "UPDATE Exam_mark SET Letter_Grade = " +
+                "CASE " +
+                "WHEN final_mark >= 90 THEN 'A+' " +
+                "WHEN final_mark >= 85 THEN 'A' " +
+                "WHEN final_mark >= 80 THEN 'A-' " +
+                "WHEN final_mark >= 75 THEN 'B+' " +
+                "WHEN final_mark >= 70 THEN 'B' " +
+                "WHEN final_mark >= 65 THEN 'B-' " +
+                "WHEN final_mark >= 60 THEN 'C+' " +
+                "WHEN final_mark >= 55 THEN 'C' " +
+                "WHEN final_mark >= 50 THEN 'C-' " +
+                "WHEN final_mark >= 45 THEN 'D+' " +
+                "WHEN final_mark >= 40 THEN 'D' " +
+                "WHEN final_mark >= 35 THEN 'E' " +
+                "ELSE 'E*' " +
+                "END, " +
+                "Grade = " +
+                "CASE " +
+                "WHEN final_mark >= 90 THEN 4.0 " +
+                "WHEN final_mark >= 85 THEN 4.0 " +
+                "WHEN final_mark >= 80 THEN 3.7 " +
+                "WHEN final_mark >= 75 THEN 3.3 " +
+                "WHEN final_mark >= 70 THEN 3.0 " +
+                "WHEN final_mark >= 65 THEN 2.7 " +
+                "WHEN final_mark >= 60 THEN 2.3 " +
+                "WHEN final_mark >= 55 THEN 2.0 " +
+                "WHEN final_mark >= 50 THEN 1.7 " +
+                "WHEN final_mark >= 45 THEN 1.3 " +
+                "WHEN final_mark >= 40 THEN 1.0 " +
+                "WHEN final_mark >= 35 THEN 0.7 " +
+                "ELSE 0.0 " +
+                "END";
+
+
+        try(Statement st = conn.createStatement()){
+            st.executeUpdate(grade);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        String credit2 = "UPDATE Exam_Mark SET Credit_gained = Grade*2.0 WHERE (Course_id = 'ICT01') || (Course_id = 'ICT02')";
+        String credit3 = "UPDATE Exam_Mark SET Credit_gained = Grade*3.0 WHERE (Course_id = 'ICT03') || (Course_id = 'ICT04') || (Course_id = 'ICT05') || (Course_id = 'ICT06')";
+
+        try (Statement c2 = conn.createStatement()){
+            c2.executeUpdate(credit2);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        try(Statement c3 = conn.createStatement()){
+            c3.executeUpdate(credit3);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 
