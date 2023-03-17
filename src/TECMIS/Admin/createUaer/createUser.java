12a2f6980f3b42776cdc84240683e26c9a464252
vinodkavinda <126967272+vinodkavinda@users.noreply.github.com>
2023-03-17 08:38:26 +0530

    admin
    form page

---
+++ src/TECMIS/Admin/createUaer/createUser.java
@@ -0,0 +1,14 @@
+package TECMIS.Admin.createUaer;
+
+import javax.swing.*;
+
+public class createUser {
+    private JTextArea facultyOfTechnologyManagementTextArea;
+    private JLabel admin;
+    private JComboBox selectBox;
+    private JButton submitButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
